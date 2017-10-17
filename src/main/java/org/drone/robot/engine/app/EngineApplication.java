ee6eb32b64644e42b7c177ad11dae16ed33d0805
ankit <ankitshahit09@gmail.com>
2017-10-18 01:02:17 +0530

    testing for jenkins job trigger

--- src/main/java/org/drone/robot/engine/app/EngineApplication.java
+++ src/main/java/org/drone/robot/engine/app/EngineApplication.java
@@ -8,6 +8,7 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 public class EngineApplication {
 	public static void main(String[] args) throws GpioException,
 			InterruptedException {
+		System.out.println("Application has started ");
 		System.out.println("Total number of output address(es) provided: "
 				+ args);
 
@@ -19,7 +20,7 @@ public class EngineApplication {
 		GpioPinDigitalOutput[] outputPins = new GpioPinDigitalOutput[address.length];
 
 		outputPins = Pi4jFactory.registerAllDigitalOutputPins(address);
-		System.out.println("output : "+outputPins);
+		System.out.println("output : " + outputPins);
 		for (GpioPinDigitalOutput gpioPinDigitalOutput : outputPins) {
 			gpioPinDigitalOutput.blink(300);
 			Thread.sleep(300);
